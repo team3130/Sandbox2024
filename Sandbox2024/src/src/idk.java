aef1a30e64c34bbb7a769d7ced73e28d8d367cda
Umar K <umarkhanayatampa@gmail.com>
2024-11-13 18:08:57 -0600

    changing stuff to take screenshots for preseason lessons

--- Sandbox2024/src/src/idk.java
+++ Sandbox2024/src/src/idk.java
@@ -1,2 +1,9 @@
 public class idk {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // this is a single line comment
+        /* this is a multi line comment
+        comment ends on this line*/
+        System.out.println("Comments ended");
+    }
 }
