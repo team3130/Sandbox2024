1af78d4b34a3ca84b0faf9ecfc19f80fa959f053
GavinCole002 <gavincole1209@gmail.com>
2024-11-20 22:35:29 -0600

    fixed get issues and tryed someother things but gave up

--- Sandbox2024/src/src/animal.java
+++ Sandbox2024/src/src/animal.java
@@ -5,6 +5,7 @@ public class animal {
     public Double height;
     public int width;
     public double volume;
+    public boolean wins;
 
     public animal(int length, String name,Double height, int width ) {
         this.length = length;
@@ -13,6 +14,11 @@ public class animal {
         this.width = width;
 
     }
+    public boolean getWins()
+    {return wins;}
+    public String getName()
+
+    {return name + " wins";}
 
     public Double getHeight() {
         return height;
@@ -37,13 +43,21 @@ public class animal {
         animal fish = new animal(3, "bubbles", 1.7, 4 );
         animal penguin = new animal(23, "Fuzzy", 2.3, 7 );
         animal dinosaur = new animal(90, "dino", 20.5, 10);
-    double getVolume;
-        System.out.println("dinosaur vs penguin");
-                if (dinosaur.getVolume(dinosaur.getLength(),dinosaur.getHeight(),dinosaur.getWidth()) > penguin.getVolume(penguin.getLength(), pw)) System.out.println("dinosaur wins");
-                else (dinosaur (getVolume) < penguin(getVolume)) System.out.println("penguin wins");
 
+        System.out.println("dinosaur vs penguin");
+                if (dinosaur.getVolume(dinosaur.getLength(),dinosaur.getHeight(),dinosaur.getWidth()) > penguin.getVolume(penguin.getLength(),penguin.getHeight(),penguin.getWidth()))
+                    System.out.println(dinosaur.getName());
+
+                else System.out.println(penguin.getName());
+
+        System.out.println("fish vs cat");
+            if (fish.getVolume(fish.getLength(),fish.getHeight(),fish.getWidth()) > cat.getVolume(cat.getLength(),cat.getHeight(),cat.getWidth()))
+                System.out.println(fish.getName());
+
+            else System.out.println(cat.getName());
 
 
     }
+    }
 
-}
+
