5873df43916e9db6d87751f2b9854395bbae6757
TreyMichalski <151589943+TreyMichalski@users.noreply.github.com>
2024-11-22 12:30:03 -0600

    sdasdas
    asdasda

--- Sandbox2024/src/src/ExampleClasses/Baby_Mohnish.java
+++ Sandbox2024/src/src/ExampleClasses/Baby_Mohnish.java
@@ -12,8 +12,9 @@ public class Baby_Mohnish {
     public boolean uses_DotPom;
     public double dangerLevel;
     public String zodiacSign;
+    public boolean isKeeper;
 
-    public Baby_Mohnish(String catch_Phrase, String ethnicity, int age, double height, boolean good_At_Basketball, boolean cool, double width, String nick_Name, boolean uses_DotPom, double dangerLevel, String zodiacSign) {
+    public Baby_Mohnish(String catch_Phrase, String ethnicity, int age, double height, boolean good_At_Basketball, boolean cool, double width, String nick_Name, boolean uses_DotPom, double dangerLevel, String zodiacSign, boolean isKeeper) {
         this.catch_Phrase = catch_Phrase;
         this.ethnicity = ethnicity;
         this.height = height;
@@ -25,12 +26,10 @@ public class Baby_Mohnish {
         this.dangerLevel = dangerLevel;
         this.age = age;
         this.zodiacSign = zodiacSign;
+        this.isKeeper = isKeeper;
     }
 
-    public boolean setDotPom(boolean usesDotPom) {
-        this.uses_DotPom = usesDotPom;
-        return uses_DotPom;
-    }
+
 
     public boolean getDotPom() {
         return uses_DotPom;
@@ -38,13 +37,42 @@ public class Baby_Mohnish {
 
     public boolean isCoolOnlyIfUsesDotPom() {
         if (uses_DotPom) {
-            System.out.println("Cool!");
+            System.out.println("He uses .pom! Awesome Sauce!");
             return cool = true;
         } else {
-            System.out.println("uncool dude");
+            System.out.println("He doesn't use .pom. uncool dude.");
             return cool = false;
         }
     }
+
+    public String getZodiacSign() {
+        return zodiacSign;
+    }
+
+    public void isHeAKeeper() {
+        if (zodiacSign == "caprisun") {
+            System.out.println("Caprisun! That's a keeper!");
+        } else {
+            System.out.println(isDeathPenalty());
+        }
+    }
+    public boolean isDeathPenalty(){
+    if (zodiacSign == "hot") {
+        System.out.println("Hot! That's a keeper!");
+        return isKeeper = true;
+        } else {
+        System.out.println("Death penalty.");
+        return isKeeper = false;
+        }
+    }
+    public void isCatchPhraseCool(){
+        if (catch_Phrase == "bruh") {
+            System.out.println("Catch Phrase Analysis = stupid catch phrase, death penalty");
+        } else {
+            System.out.println("Catch Prase Analysis = tippity top!");
+        }
+    }
 }
+
 
 
--- Sandbox2024/src/src/ExampleClasses/The_Mohnish_Machine.java
+++ Sandbox2024/src/src/ExampleClasses/The_Mohnish_Machine.java
@@ -2,39 +2,53 @@ package ExampleClasses;
 
 public class The_Mohnish_Machine {
     public static void main(String[] args) {
-        Baby_Mohnish normalMohnish = new Baby_Mohnish("Attention! bruh.", "INDIA", 18, 6.1, true, true, 1.5, "sly dog", true, 75, "");
-        Baby_Mohnish babyMohnish = new Baby_Mohnish("Bruh", "mixed", 3, 3.5, false, false, 0.7, "good boy", false, 10, "aquarius");
-        Baby_Mohnish superMohnish = new Baby_Mohnish("Super hero sized bite! For a super hero size Appetite!", "obviosly white", 20, 7.2, true, true, 5, "Monster Mohnish", true, 100, "capricorn");
-        Baby_Mohnish middleAgeMohnish = new Baby_Mohnish("Bone digadone!", "blasian", 34, 5.3, false, true, 1, "", true, 90, "chungus");
+        Baby_Mohnish normalMohnish = new Baby_Mohnish("Attention! bruh.", "INDIA", 18, 6.1, true, true, 1.5, "sly dog", true, 75, "caprisun", true);
+        Baby_Mohnish babyMohnish = new Baby_Mohnish("Bruh", "mixed", 3, 3.5, false, false, 0.7, "good boy", false, 10, "aquarium", false);
+        Baby_Mohnish superMohnish = new Baby_Mohnish("Super hero sized bite! For a super hero sized Appetite!", "obviously white", 20, 7.2, true, true, 5, "Monster Mohnish", true, 100, "tourist", false);
+        Baby_Mohnish middleAgeMohnish = new Baby_Mohnish("Bone digadone!", "blasian", 34, 5.3, false, true, 1, "", true, 90, "hot",  true);
 
         System.out.println(
-
-
         "Normal Mohnish"
         );
-        System.out.println(normalMohnish.getDotPom());
+
+
         normalMohnish.isCoolOnlyIfUsesDotPom();
+        normalMohnish.isHeAKeeper();
+        normalMohnish.isCatchPhraseCool();
+
+        System.out.println();
         System.out.println(
-
-
-
+        "Baby Mohnish"
         );
-        System.out.println(babyMohnish.getDotPom());
+
+
         babyMohnish.isCoolOnlyIfUsesDotPom();
+        babyMohnish.isHeAKeeper();
+
+
+        System.out.println();
         System.out.println(
-
-
-
+        "Super Mohnish"
         );
-        System.out.println(superMohnish.getDotPom());
+
+
         superMohnish.isCoolOnlyIfUsesDotPom();
+        superMohnish.isHeAKeeper();
+
+
+        System.out.println();
         System.out.println(
-
-
-
+        "Middle Aged Mohnish"
         );
-        System.out.println(middleAgeMohnish.getDotPom());
+
+
         middleAgeMohnish.isCoolOnlyIfUsesDotPom();
+
+        middleAgeMohnish.isHeAKeeper();
+
+
+
+
     }
 
 }
